147d3d8f364354c818d049b0454eec82a2a2a9de
PapKate <papkate.lessons@gmail.com>
2023-11-13 11:46:18 +0200

    Exercise 1 done!

---
+++ LearningWithActors/SchoolTime/DataModels/PersonDataModel.cs
@@ -0,0 +1,43 @@
+﻿namespace SchoolTime
+{
+    /// <summary>
+    /// Represents a person
+    /// </summary>
+    public class PersonDataModel
+    {
+        #region Protected Members
+
+        /// <summary>
+        /// The age
+        /// </summary>
+        protected uint mAge;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public PersonDataModel() : base()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Prints a greeting message 
+        /// </summary>
+        public void Greet() => Console.WriteLine("Hello world!");
+
+        /// <summary>
+        /// Sets the age for the current <see cref="PersonDataModel"/>
+        /// </summary>
+        public void SetAge(uint age) => mAge = age;
+
+        #endregion
+    }
+}
---
+++ LearningWithActors/SchoolTime/DataModels/TeacherDataModel.cs
@@ -0,0 +1,29 @@
+﻿namespace SchoolTime
+{
+    /// <summary>
+    /// Represents a teacher
+    /// </summary>
+    public class TeacherDataModel : PersonDataModel
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TeacherDataModel() : base()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Prints a message notifying that the current <see cref="StudentDataModel"/> is studying
+        /// </summary>
+        public void Explain() => Console.WriteLine("I'm explaining.");
+
+        #endregion
+    }
+}
