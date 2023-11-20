31f95806273f9bbafc341bd717b3ca265e7dbb6e
PapKate <papkate.lessons@gmail.com>
2023-11-20 15:30:42 +0200

    Completion of assignment two

---
+++ LearningWithActors/AlbumTime/PhotoBookTest.cs
@@ -0,0 +1,20 @@
+﻿namespace AlbumTime
+{
+    public static class PhotoBookTest
+    {
+        public static void TestingTime()
+        {
+            Console.WriteLine("________________");
+            var defaultPhotoBook = new PhotoBookDataModel();
+            Console.WriteLine($"This photo book has {defaultPhotoBook.GetNumberPages()} pages.");
+            Console.WriteLine("________________");
+            var standardPhotoBook = new PhotoBookDataModel(24);
+            Console.WriteLine($"This photo book has {standardPhotoBook.GetNumberPages()} pages.");
+            Console.WriteLine("________________");
+            var bigPhotoBook = new BigPhotoBookDataModel();
+            Console.WriteLine($"This photo book has {bigPhotoBook.GetNumberPages()} pages.");
+            Console.WriteLine("________________");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LearningWithActors/AlbumTime/Program.cs
@@ -0,0 +1,3 @@
+﻿using AlbumTime;
+
+PhotoBookTest.TestingTime();
